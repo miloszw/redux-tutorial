3ae3c23544668311a3c5c23332ad2b8acd841e68
Milosz Wielondek <miloszw@kth.se>
2017-02-06 23:15:34 +0100

    Subscribe to store changes and listen for con

--- voting-server/src/server.js
+++ voting-server/src/server.js
@@ -1,5 +1,16 @@
 import Server from 'socket.io';
 
-export default function startServer() {
+export default function startServer(store) {
   const io = new Server().attach(8090);
+
+  store.subscribe(
+    // TODO currently sending the whole state, look into
+    // only sending the relevant subset or a diff
+    () => io.emit('state', store.getState().toJS())
+  );
+
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJS());
+  });
+  
 }
